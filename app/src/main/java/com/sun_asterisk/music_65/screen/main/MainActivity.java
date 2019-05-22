9600ba00f01d1af2990f21f9e8368fa8b34cb68a
lylyonl <vumhung1997@gmail.com>
2019-05-22 09:22:19 +0700

    create base project

---
+++ app/src/main/java/com/sun_asterisk/music_65/screen/main/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sun_asterisk.music_65.screen.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.sun_asterisk.music_65.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
